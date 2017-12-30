c2b64cfe2dbe4137c39725b72ce719980d6ef9b6
Paul Halliday <hello@paulhalliday.io>
2017-12-30 19:23:24 +0000

    Basic counter example

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>{{count}}</h1>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    increment() {
+      this.$store.commit('increment', this.count);
+    },
+    decrement() {
+      this.$store.commit('decrement', this.count);
+    },
+  },
+  computed: {
+    count() {
+      return this.$store.state.count;
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
